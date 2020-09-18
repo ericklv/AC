c24f9c030ead72c59cb88bdf12ea3ff89a37a9d0
thanatos <elazon01@gmail.com>
2020-09-18 11:56:55 -0500

    add calculate_reverse

--- src/business/rules/index.js
+++ src/business/rules/index.js
@@ -5,21 +5,21 @@ export const rule30 = (initial, time) => ruleX(initial, time, rule30_f);
 
 export const random_generator = lenght => {
     let output = "";
-    let rule30_ = rule30("000000010000000", lenght+1);
-    let pos_x =rule30_[0].indexOf("1");
+    let rule30_ = rule30("000000010000000", lenght + 1);
+    let pos_x = rule30_[0].indexOf("1");
 
-    for (let i = 0; i < lenght; i++) output += rule30_[i+1][pos_x]
+    for (let i = 0; i < lenght; i++) output += rule30_[i + 1][pos_x]
     return output;
 };
 
-export const prime_generator = (lenght, initial = "000000010000000") => {
+export const prime_generator = (lenght, initial) => {
     let output = "";
-    let rule30_ = rule30(initial, lenght-1);
-    let pos_x =rule30_[0].indexOf("1");
+    let rule30_ = rule30(initial, lenght - 1);
+    let pos_x = Math.floor((rule30_[0].length) / 2);
 
-    for (let i = 0; i < lenght-1; i++) output += rule30_[i+1][pos_x]
-    console.log("bf",output.length)
-    return "1"+output+"1";
+    for (let i = 0; i < lenght - 1; i++) output += rule30_[i + 1][pos_x]
+    console.log("bf", output.length)
+    return "1" + output + "1";
 };
 
 export const rule45 = (initial, time) => {
@@ -36,7 +36,7 @@ export const vernam = (text) => {
     console.log("out", output);
     console.log(bin_to_string(output))
     console.log(bin_to_hex(output))
-    console.log("prime",prime_generator(128,"0010100101100111100101110" ))
+    console.log("prime", prime_generator(256, "0010100101100111100101110"));
     return output;
 }
 
@@ -65,7 +65,7 @@ export const bin_to_string = bin_ => {
     let bin_array = bin_.match(/.{1,8}/g);
     let output = "";
 
-    bin_array.forEach( arr => {
+    bin_array.forEach(arr => {
         output += String.fromCharCode(parseInt(arr, 2));
     })
     return output;
@@ -109,10 +109,60 @@ const fix_initial_state = (initial, time) => {
     }
 };
 
-const check_prime = (binary) => {
-    if (binary[0] !== "0") return binary;
-    else return "1" + binary;
+const rule30_f = (a_i, a_i_1 = 0, a_i__1 = 0) => a_i__1 ^ (a_i | a_i_1);
+const rule45_f = (a_i, a_i_1 = 0, a_i__1 = 0) => a_i__1 ^ (a_i | !(a_i_1));
+
+export const calculate_reverse = (seed, initial ) => {
+    let seed_ = string2num(seed);
+    let recovery  = new Array(initial.length);
+    let intents = 0;
+    while (compare_arrays(seed_, recovery[0])){
+        recovery = reverse(initial);
+        ++intents;
+    }
+    return intents;
 };
 
-const rule30_f = (a_i, a_i_1 = 0, a_i__1 = 0) => a_i__1 ^ (a_i | a_i_1);
-const rule45_f = (a_i, a_i_1 = 0, a_i__1 = 0) => a_i__1 ^ (a_i | (a_i_1));
+export const reverse = initial => {
+    let n = initial.length;
+    let recovery = new Array(n);
+
+    for (let x = 0; x < n; x++) {
+        recovery[x] = new Array(2 * n - 1)
+        for (let y = 0; y < 2 * n - 1; y++) {
+            recovery[x][y] = 0
+        }
+    }
+
+    for (let j = 0; j < n; j++) {
+        recovery[0][n + j + 1] =  Math.round(Math.random());
+        recovery[j][n] = initial.charAt(j);
+    }
+
+    for (let k = 0; k < n - 1; k++) {
+        for (let j = 0; j < n - k; j++) {
+            recovery[k + 1][n + j] = rule30_f(recovery[k][n + j], recovery[k][n + j + 1], recovery[k][n + j - 1])
+        }
+    }
+
+    for (let j = 0; j < n; j++) {
+        for (let k = n - j - 1; k > 0; k--) {
+            recovery[k][n - j - 1] = rule30_f(recovery[k][n - j + 1], recovery[k][n - j + 2], recovery[k + 1][n - j + 1])
+        }
+    }
+
+    return recovery;
+};
+
+const compare_arrays = (a, b) => {
+    return Array.isArray(a) &&
+        Array.isArray(b) &&
+        a.length === b.length &&
+        a.every((val, index) => val === b[index]);
+}
+
+const string2num =  val => val.split('').map(num => {
+    let n = Number(num);
+    return n === 0 ? n : n || num;
+});
+
